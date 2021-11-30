42a66ed4fc0bc1ea20215f55123c4f28d008e4a7
Susan Cheng <susan.doggie@gmail.com>
2021-11-30 10:27:25 +0800

    init

---
+++ Icons/index.js
@@ -0,0 +1,42 @@
+//
+//  index.js
+//
+//  The MIT License
+//  Copyright (c) 2015 - 2021 Susan Cheng. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import './font-inject';
+
+export const AntDesign = require('react-native-vector-icons/dist/AntDesign');
+export const Entypo = require('react-native-vector-icons/dist/Entypo');
+export const EvilIcons = require('react-native-vector-icons/dist/EvilIcons');
+export const Feather = require('react-native-vector-icons/dist/Feather');
+export const FontAwesome = require('react-native-vector-icons/dist/FontAwesome');
+export const FontAwesome5 = require('react-native-vector-icons/dist/FontAwesome5');
+export const FontAwesome5Pro = require('react-native-vector-icons/dist/FontAwesome5Pro');
+export const Fontisto = require('react-native-vector-icons/dist/Fontisto');
+export const Foundation = require('react-native-vector-icons/dist/Foundation');
+export const Ionicons = require('react-native-vector-icons/dist/Ionicons');
+export const MaterialCommunityIcons = require('react-native-vector-icons/dist/MaterialCommunityIcons');
+export const MaterialIcons = require('react-native-vector-icons/dist/MaterialIcons');
+export const Octicons = require('react-native-vector-icons/dist/Octicons');
+export const SimpleLineIcons = require('react-native-vector-icons/dist/SimpleLineIcons');
+export const Zocial = require('react-native-vector-icons/dist/Zocial');
