e2e7ae2e258473a155c05e1bca067e0299b7ac21
Susan Cheng <susan.doggie@gmail.com>
2022-01-20 22:19:31 +0800

    fix

--- index.js
+++ index.js
@@ -23,7 +23,6 @@
 //  THE SOFTWARE.
 //
 
-export { default as styleInject } from './styleInject';
 export { default as BBCode } from './BBCode';
 export { default as Button } from './Button';
 export { default as HTML } from './HTML';
