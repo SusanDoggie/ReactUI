8de3523bc8349545c62c3864e97edaa9a9781341
Susan Cheng <susan.doggie@gmail.com>
2022-02-13 14:24:29 +0800

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,21 @@
 
+const packageJson = require('./package.json');
+
 module.exports = {
+    project: {
+      ios: {},
+      android: {},
+    },
     dependency: {
       platforms: {
         ios: {},
         android: {},
       },
     },
+    dependencies: packageJson.dependencies.mapValues(() => ({
+        platforms: {
+            ios: {},
+            android: {},
+        }
+    })),
 };
